081f6a06fdc5e502b9d59d7a2030e057eeca5a6e
mhsalamehop <mohammad.salameh@optimumpartners.co>
2023-05-18 13:49:22 +0300

    added cli that backfills the database with moviedb data using an upstream query

--- cli/helpers.go
+++ cli/helpers.go
@@ -1,17 +1,16 @@
-package controllers
+package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"strings"
 
-	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
 	"github.com/mhsalamehop/go-api/models"
-	"github.com/mhsalamehop/go-api/store"
 	"github.com/mhsalamehop/go-api/utils"
 )
 
@@ -22,74 +21,71 @@ type MovieResult struct {
 	TotalResults int64           `json:"total_results"`
 }
 
-func GetMovies(w http.ResponseWriter, r *http.Request) {
-	db:=store.OpenConnection()
-	// sli := []string{"id","overview","title"}
-	valueStrings := []string{}
-	valueArgs := []interface{}{}
-	txn,err := db.Begin()
+func GetMoviesFromAPI() (MovieResult, error) {
+	err := godotenv.Load(".env")
 	if err != nil {
-		utils.LogInfo(err.Error())
+		utils.LogError("loading .env file with err %s", err.Error())
 	}
-
-	// var movie models.Movies
 	url := "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
 	token := os.Getenv("MYMOVIEDB_TOKEN")
 	client := http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
+	var movieResult MovieResult
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		utils.LogError(err.Error())
+		return movieResult, err
 	}
 	req.Header.Add("accept", "application/json")
 	req.Header.Add("Authorization", "Bearer "+token)
 
 	res, err := client.Do(req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		utils.LogError(err.Error())
+		return movieResult, err
 	}
 	defer res.Body.Close()
 
-	var movieResult MovieResult
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		utils.LogError(err.Error())
+		return movieResult, err
 	}
 	err = json.Unmarshal(body, &movieResult)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		utils.LogError(err.Error())
+		return movieResult, err
 	}
-	c:=0
-	for _, w := range  movieResult.Results{
-		valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d)", c+1, c+2, c+3))    
-		valueArgs = append(valueArgs, w.Id)
-		valueArgs = append(valueArgs, w.Title)
-		valueArgs = append(valueArgs, w.Overview)
-		c+=3
-	  }
-	smt := `INSERT INTO api_movies (id, title, overview) VALUES %s ON CONFLICT (id) DO UPDATE SET overview = excluded.overview`
-	smt = fmt.Sprintf(smt, strings.Join(valueStrings, ", "))
-	_,err = txn.Exec(smt, valueArgs...)
-	if err != nil {
-		utils.LogInfo(err.Error())
-	}
-
-	err = txn.Commit()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data, err := json.Marshal(movieResult.Results)
-	
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(data)
+	return movieResult, nil
 
 }
 
-
+func BackFill(movieResult MovieResult, db *sql.DB) error {
+	valueStrings := []string{}
+	valueArgs := []interface{}{}
+	txn, err := db.Begin()
+	if err != nil {
+		utils.LogError(err.Error())
+		return err
+	}
+	c := 0
+	for _, w := range movieResult.Results {
+		valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d)", c+1, c+2, c+3))
+		valueArgs = append(valueArgs, w.Id)
+		valueArgs = append(valueArgs, w.Title)
+		valueArgs = append(valueArgs, w.Overview)
+		c += 3
+	}
+	smt := `INSERT INTO api_movies (id, title, overview) VALUES %s ON CONFLICT (id) DO UPDATE SET overview = excluded.overview`
+	smt = fmt.Sprintf(smt, strings.Join(valueStrings, ", "))
+	_, err = txn.Exec(smt, valueArgs...)
+	if err != nil {
+		utils.LogError(err.Error())
+		return err
+	}
+	err = txn.Commit()
+	if err != nil {
+		utils.LogError(err.Error())
+		return err
+	}
+	return nil
+}
--- schema/schema.sql
+++ schema/schema.sql
@@ -1,10 +1,17 @@
 DROP TABLE users_table;
 DROP TABLE saved_movies;
+DROP TABLE api_movies;
 CREATE TABLE IF NOT EXISTS saved_movies (
     id SERIAL PRIMARY KEY,
     title text,
     overview text,
     saved_info json
+);
+DROP TABLE api_movies;
+CREATE TABLE IF NOT EXISTS api_movies (
+    id text PRIMARY KEY,
+    title text,
+    overview text
 );
 CREATE TABLE IF NOT EXISTS users_table (
     id SERIAL PRIMARY KEY,
